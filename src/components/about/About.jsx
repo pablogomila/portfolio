323ec90eb913129edb7357c3d37a535ddffdfc88
Pablo Gomila <pablogiphone@icloud.com>
2021-09-08 20:43:10 -0300

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,25 @@
-import "./about.scss"
+import './about.scss'
+import techstack from '../../assets/techstack.gif'
+import PermIdentityIcon from '@material-ui/icons/PermIdentity'
 
 const About = () => {
-    return (
-        <div className="about" id="about">
-            About
+  return (
+    <div className="about" id="about">
+      <div className="left">
+        <div class="glitch" data-text="ABOUT ME">
+          ABOUT ME
+          <PermIdentityIcon className="icon" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div class="glitch" data-text="TECH STACK">
+          TECH STACK
+          <PermIdentityIcon className="icon" />
+        </div>
+        <img src={techstack} alt="" />
+      </div>
+    </div>
+  )
 }
 
 export default About
